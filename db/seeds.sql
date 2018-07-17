0e2944f4f7ee7661209b135870c0a98070863967
Brian Lynch <brianlynch@Brians-MacBook-Pro.local>
2018-07-17 18:50:41 -0400

    rough draft

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwestern SmokeHouse', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black and Blue', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
